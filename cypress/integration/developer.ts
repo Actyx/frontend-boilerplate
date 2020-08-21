2be35b258e736e6b96a611466ba9c569702f13cd
GibboK <gibbok.coding@gmail.com>
2020-08-21 18:17:16 +0200

    save

--- cypress/integration/developer.ts
+++ cypress/integration/developer.ts
@@ -1,59 +1,22 @@
 /* eslint-disable prefer-const */
-import { filterInternalUrls, getUrlsFromDocument, removeHashFromUrl } from './utils';
+import { getUrlsFromDocument, removeHashFromUrl } from './utils';
 
-const BASE_URL = 'https://developer.actyx.com/';
-const VISIT_TIMEOUT = 10 * 1000;
+const BASE_URL = 'https://developer.actyx.com/docs/cli/getting-started';
 
 describe('developer.com', () => {
   it('should walk website and detect broken links', () => {
-    let urlsQue: string[] = [];
-    let urlsExternal: string[] = [];
-
     cy.visit(BASE_URL);
-
     cy.document().then((doc) => {
-      urlsQue = getUrlsFromDocument(doc);
-
-      const checkUrls = () => {
-        let next = urlsQue.pop();
-        if (next) {
-          const isInternal = next.startsWith(BASE_URL);
-          if (isInternal) {
-            next = removeHashFromUrl(next);
-            cy.visit({
-              url: next,
-              failOnStatusCode: true,
-              timeout: VISIT_TIMEOUT,
-            });
-            const urlsVisit = getUrlsFromDocument(document);
-            const urlUnique = filterInternalUrls(urlsQue, urlsVisit, BASE_URL);
-            urlsQue = [...urlsQue, ...urlUnique];
-            checkUrls();
-          } else {
-            urlsExternal.push(next);
-            checkUrls();
-          }
-        } else {
-          console.log('DONE!');
-          console.log(urlsExternal);
-        }
-      };
-
-      checkUrls();
+      getUrlsFromDocument(doc)
+        .filter((x) => x.startsWith(BASE_URL))
+        .map(removeHashFromUrl)
+        .filter((x: string, idx: number, data: string[]) => data.indexOf(x) === idx)
+        .forEach((x) => {
+          console.log(x);
+          // cy.visit(x).then((x) => {
+          //   console.log(x.document.title);
+          // });
+        });
     });
   });
 });
-
-/*
-
-// GET ALL LINKS
-- get all links from homepage and add to que
-- take first element from the que
-- if url start with developer continue else skip
-- take all links from page taken from the que
-- for each linq, add to quey only if it is not alreay present
-- continue from start
-- stop when que is empty
-
-
-*/
--- cypress/integration/utils.ts
+++ cypress/integration/utils.ts
@@ -4,9 +4,3 @@ export const getUrlsFromDocument = (doc: Document): string[] =>
   Array.from(doc.querySelectorAll('a'))
     .map((x) => x.href)
     .filter((x) => x.length > 0);
-
-export const filterInternalUrls = (
-  prevUrls: string[],
-  newUrls: string[],
-  baseUrl: string
-): string[] => newUrls.filter((x) => !prevUrls.includes(x) && x.startsWith(baseUrl));
