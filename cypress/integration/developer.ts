f4df7743dd660fe9f705e6dcc72ccf1e08c9a716
GibboK <gibbok.coding@gmail.com>
2020-08-21 15:20:22 +0200

    Take next url from page and visit

--- cypress/integration/developer.ts
+++ cypress/integration/developer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 const BASE_URL = 'https://developer.actyx.com/';
 
 describe('developer.com', () => {
@@ -17,6 +18,61 @@ describe('developer.com', () => {
           url: url,
         });
       });
+
+      //
+    });
+  });
+
+  const getUrlsFromDOM = (doc: Document) =>
+    Array.from(doc.querySelectorAll('a'))
+      .map((x) => x.href)
+      .filter((x) => x.length > 0);
+
+  it.only('should detect recursivelly', () => {
+    let que: string[] = [];
+
+    cy.visit(BASE_URL);
+    cy.document().then((doc) => {
+      que = getUrlsFromDOM(doc);
+      const next = que.pop();
+      if (next) {
+        const isInternal = next.startsWith(BASE_URL);
+        if (isInternal) {
+          cy.visit({ url: next }).then((x) => {
+            console.log('ready');
+            const newItems = getUrlsFromDOM(x.document);
+            const filtered = newItems.filter((x) => !que.includes(x));
+            que = [...que, ...filtered];
+          });
+        } else {
+          console.log('not internal');
+        }
+      }
+      // while (que.length > 0) {
+      //   const next = que.pop();
+      //   if (next) {
+      //     const isInternal = next.startsWith(BASE_URL);
+      //     if (isInternal) {
+      //       cy.visit({ url: next });
+      //     } else {
+      //       que.push(next);
+      //     }
+      //   }
+      // }
     });
   });
 });
+
+/*
+
+// GET ALL LINKS
+- get all links from homepage and add to que
+- take first element from the que
+- if url start with developer continue else skip
+- take all links from page taken from the que
+- for each linq, add to quey only if it is not alreay present
+- continue from start
+- stop when que is empty
+
+
+*/
