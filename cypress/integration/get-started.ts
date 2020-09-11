4412f62bf70cd21cd749ecdd8089a1c016a04df2
GibboK <gibbok.coding@gmail.com>
2020-09-11 11:16:10 +0200

    Add test integation get started eng

---
+++ cypress/integration/get-started.ts
@@ -0,0 +1,34 @@
+import { mkRandomEmail, mkRandomString } from '../utils';
+
+const TEST_EMAIL = mkRandomEmail();
+const TEST_DETAILS = `test - ${mkRandomString()} - I am a robot adding details`;
+
+describe('get-started', () => {
+  before(() => {
+    cy.visit('https://www.actyx.com/get-started/');
+  });
+
+  it('should schedule a call for a factory manager', () => {
+    // find button on page
+    const btnScheduleCall = cy.contains('Schedule a call');
+    btnScheduleCall.should('exist');
+
+    // click button and open dialog
+    btnScheduleCall.click();
+    const inputEmail = cy.get('input[name="email"]').should('exist');
+
+    // enter email
+    inputEmail.click();
+    inputEmail.type(TEST_EMAIL);
+
+    // enter details
+    const inputDetails = cy.get('textarea[name="additionalDetails"]').should('exist');
+    inputDetails.type(TEST_DETAILS);
+
+    // submit form
+    const btnSumbit = cy.get('button[type="submit"]').should('exist');
+    btnSumbit.click();
+    cy.contains('Thank you!').should('exist');
+    //https://www.actyx.com/thank-you/
+  });
+});
