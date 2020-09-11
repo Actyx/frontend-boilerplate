eb5c4f0aeb492c7b8d1fbeaf8b1b3e7c99f07181
GibboK <gibbok.coding@gmail.com>
2020-09-11 11:44:52 +0200

    Add test redirect user

--- cypress/integration/get-started.ts
+++ cypress/integration/get-started.ts
@@ -1,7 +1,8 @@
 import { mkRandomEmail, mkRandomString } from '../utils';
 
 const URL_START = 'https://www.actyx.com/get-started/';
-const URL_END = 'https://www.actyx.com/thank-you';
+const URL_END_THANKS = 'https://www.actyx.com/thank-you';
+const URL_END_DEVELOPER = 'https://developer.actyx.com/docs/home/welcome';
 const TEST_EMAIL = mkRandomEmail();
 const TEST_DETAILS = `test - ${mkRandomString()} - I am a robot adding details`;
 
@@ -9,7 +10,7 @@ const checkThankYou = () => {
   // assert submit was successful
   cy.contains('Thank you!').should('exist');
   cy.location().should((loc) => {
-    expect(loc.href).to.eq(URL_END);
+    expect(loc.href).to.eq(URL_END_THANKS);
   });
 };
 
@@ -42,11 +43,11 @@ describe('get-started', () => {
   it('should schedule a demo for an it project manager', () => {
     cy.visit(URL_START);
     // find button on page
-    const btnScheduleCall = cy.contains('Get a demo');
-    btnScheduleCall.should('exist');
+    const btnGetDemo = cy.contains('Get a demo');
+    btnGetDemo.should('exist');
 
     // click button and open dialog
-    btnScheduleCall.click();
+    btnGetDemo.click();
     const inputEmail = cy.get('input[name="email"]').should('exist');
 
     // enter email
@@ -62,5 +63,18 @@ describe('get-started', () => {
     btnSumbit.click();
 
     checkThankYou();
+  });
+
+  it.only('should redirect a developer to https://developer.actyx.com/', () => {
+    cy.visit(URL_START);
+    // find button on page
+    const btnGetStart = cy.get('button').contains('Get started');
+    btnGetStart.should('exist');
+
+    // click button and redirect user
+    btnGetStart.click();
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq(URL_END_DEVELOPER);
+    });
   });
 });
