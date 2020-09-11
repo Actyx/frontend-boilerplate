ed7173a40d6f8eea91ec905a547ee69de2607a69
GibboK <gibbok.coding@gmail.com>
2020-09-11 11:52:44 +0200

    Fix

--- cypress/integration/get-started.ts
+++ cypress/integration/get-started.ts
@@ -3,6 +3,7 @@ import { mkRandomEmail, mkRandomString } from '../utils';
 const URL_START = 'https://www.actyx.com/get-started/';
 const URL_END_THANKS = 'https://www.actyx.com/thank-you';
 const URL_END_DEVELOPER = 'https://developer.actyx.com/docs/home/welcome';
+
 const TEST_EMAIL = mkRandomEmail();
 const TEST_DETAILS = `test - ${mkRandomString()} - I am a robot adding details`;
 
@@ -14,9 +15,15 @@ const checkThankYou = () => {
   });
 };
 
+const submitForm = () => {
+  const btnSumbit = cy.get('button[type="submit"]').should('exist');
+  btnSumbit.click();
+};
+
 describe('get-started', () => {
   it('should schedule a call for a factory manager', () => {
     cy.visit(URL_START);
+
     // find button on page
     const btnScheduleCall = cy.contains('Schedule a call');
     btnScheduleCall.should('exist');
@@ -33,15 +40,14 @@ describe('get-started', () => {
     const inputDetails = cy.get('textarea[name="additionalDetails"]').should('exist');
     inputDetails.type(TEST_DETAILS);
 
-    // submit form
-    const btnSumbit = cy.get('button[type="submit"]').should('exist');
-    btnSumbit.click();
+    submitForm();
 
     checkThankYou();
   });
 
   it('should schedule a demo for an it project manager', () => {
     cy.visit(URL_START);
+
     // find button on page
     const btnGetDemo = cy.contains('Get a demo');
     btnGetDemo.should('exist');
@@ -58,15 +64,14 @@ describe('get-started', () => {
     const inputDetails = cy.get('textarea[name="additionalDetails"]').should('exist');
     inputDetails.type(TEST_DETAILS);
 
-    // submit form
-    const btnSumbit = cy.get('button[type="submit"]').should('exist');
-    btnSumbit.click();
+    submitForm();
 
     checkThankYou();
   });
 
-  it.only('should redirect a developer to https://developer.actyx.com/', () => {
+  it('should redirect a developer to developer site', () => {
     cy.visit(URL_START);
+
     // find button on page
     const btnGetStart = cy.get('button').contains('Get started');
     btnGetStart.should('exist');
