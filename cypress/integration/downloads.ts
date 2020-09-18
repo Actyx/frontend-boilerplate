945ec7fb09b12857bd3f05757aa7f7444de1793f
GibboK <gibbok.coding@gmail.com>
2020-09-18 11:34:51 +0200

    cleanup remove console.log

--- cypress/integration/downloads.ts
+++ cypress/integration/downloads.ts
@@ -2,6 +2,7 @@ const URL_START = 'https://downloads.actyx.com/';
 const URL_STORAGE = 'https://axartifacts.blob.core.windows.net/artifacts/';
 
 const FOLDER = {
+  android: '/android/',
   win64: '/win64/',
   mac: '/mac/',
   x64: '/x64/',
@@ -20,43 +21,37 @@ describe('downloads.actyx.com', () => {
         .filter((x) => x.startsWith(URL_STORAGE));
 
       // os android
-      const androids = hrefs.filter((x) => x.includes('android'));
-      console.log(androids);
+      const androids = hrefs.filter((x) => x.includes(FOLDER.android));
       androids.forEach((x) => {
         expect(x.endsWith('actyxos.apk')).to.eq(true);
       });
 
-      // os win
+      // os win64
       const win64s = hrefs.filter((x) => x.includes(FOLDER.win64));
-      console.log(win64s);
       win64s.forEach((x) => {
         expect(x.endsWith('ax.exe')).to.eq(true);
       });
 
       // mac ax
       const macs = hrefs.filter((x) => x.includes(FOLDER.mac));
-      console.log(macs);
       macs.forEach((x) => {
         expect(x.endsWith('ax')).to.eq(true);
       });
 
       // ax linux 64
       const linux64s = hrefs.filter((x) => x.includes(FOLDER.x64));
-      console.log(linux64s);
       linux64s.forEach((x) => {
         expect(x.endsWith('ax')).to.eq(true);
       });
 
       // ax aarch64
       const aarch64s = hrefs.filter((x) => x.includes(FOLDER.aarch64));
-      console.log(aarch64s);
       aarch64s.forEach((x) => {
         expect(x.endsWith('ax')).to.eq(true);
       });
 
       // ax arm
       const arm64x = hrefs.filter((x) => x.includes(FOLDER.arm));
-      console.log(arm64x);
       arm64x.forEach((x) => {
         expect(x.endsWith('ax')).to.eq(true);
       });
