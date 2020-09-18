d5e0b37c25817df91c533cce3a4e378652b0d7d3
GibboK <gibbok.coding@gmail.com>
2020-09-18 11:29:19 +0200

    Better folder structure and some cleanup

--- cypress/integration/downloads.ts
+++ cypress/integration/downloads.ts
@@ -1,4 +1,13 @@
 const URL_START = 'https://downloads.actyx.com/';
+const URL_STORAGE = 'https://axartifacts.blob.core.windows.net/artifacts/';
+
+const FOLDER = {
+  win64: '/win64/',
+  mac: '/mac/',
+  x64: '/x64/',
+  aarch64: '/aarch64/',
+  arm: '/arm/',
+};
 
 describe('downloads.actyx.com', () => {
   it('should check if artifact files name are valid', () => {
@@ -8,15 +17,49 @@ describe('downloads.actyx.com', () => {
         .toArray()
         .map((x) => x.href)
         .filter((x) => x !== '')
-        .filter((x) => x.startsWith('https://axartifacts.blob.core.windows.net/artifacts/'));
+        .filter((x) => x.startsWith(URL_STORAGE));
 
-      const android = hrefs.filter((x) => x.includes('android'));
-      android.forEach((x) => {
-        expect(x).to.includes('actyxos.apk');
+      // os android
+      const androids = hrefs.filter((x) => x.includes('android'));
+      console.log(androids);
+      androids.forEach((x) => {
+        expect(x.endsWith('actyxos.apk')).to.eq(true);
       });
-      console.log(android);
 
-      console.log(hrefs);
+      // os win
+      const win64s = hrefs.filter((x) => x.includes(FOLDER.win64));
+      console.log(win64s);
+      win64s.forEach((x) => {
+        expect(x.endsWith('ax.exe')).to.eq(true);
+      });
+
+      // mac ax
+      const macs = hrefs.filter((x) => x.includes(FOLDER.mac));
+      console.log(macs);
+      macs.forEach((x) => {
+        expect(x.endsWith('ax')).to.eq(true);
+      });
+
+      // ax linux 64
+      const linux64s = hrefs.filter((x) => x.includes(FOLDER.x64));
+      console.log(linux64s);
+      linux64s.forEach((x) => {
+        expect(x.endsWith('ax')).to.eq(true);
+      });
+
+      // ax aarch64
+      const aarch64s = hrefs.filter((x) => x.includes(FOLDER.aarch64));
+      console.log(aarch64s);
+      aarch64s.forEach((x) => {
+        expect(x.endsWith('ax')).to.eq(true);
+      });
+
+      // ax arm
+      const arm64x = hrefs.filter((x) => x.includes(FOLDER.arm));
+      console.log(arm64x);
+      arm64x.forEach((x) => {
+        expect(x.endsWith('ax')).to.eq(true);
+      });
     });
   });
 });
