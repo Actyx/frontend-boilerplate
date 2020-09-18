594ff8ebc746af29f1593a4d37f22128f815fdde
GibboK <gibbok.coding@gmail.com>
2020-09-18 11:40:49 +0200

    Improve endWith check

--- cypress/integration/downloads.ts
+++ cypress/integration/downloads.ts
@@ -11,7 +11,7 @@ const FOLDER = {
 };
 
 describe('downloads.actyx.com', () => {
-  it('should check if artifact file names are valid', () => {
+  it('should check if artifact file names and extenions are valid', () => {
     cy.visit(URL_START);
     cy.get('a').then((x) => {
       const hrefs = x
@@ -23,37 +23,37 @@ describe('downloads.actyx.com', () => {
       // os android
       const androids = hrefs.filter((x) => x.includes(FOLDER.android));
       androids.forEach((x) => {
-        expect(x.endsWith('actyxos.apk')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.android}actyxos.apk`)).to.eq(true);
       });
 
       // os win64
       const win64s = hrefs.filter((x) => x.includes(FOLDER.win64));
       win64s.forEach((x) => {
-        expect(x.endsWith('ax.exe')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.win64}ax.exe`)).to.eq(true);
       });
 
       // mac ax
       const macs = hrefs.filter((x) => x.includes(FOLDER.mac));
       macs.forEach((x) => {
-        expect(x.endsWith('ax')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.mac}ax`)).to.eq(true);
       });
 
       // ax linux 64
       const linux64s = hrefs.filter((x) => x.includes(FOLDER.x64));
       linux64s.forEach((x) => {
-        expect(x.endsWith('ax')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.x64}ax`)).to.eq(true);
       });
 
       // ax aarch64
       const aarch64s = hrefs.filter((x) => x.includes(FOLDER.aarch64));
       aarch64s.forEach((x) => {
-        expect(x.endsWith('ax')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.aarch64}ax`)).to.eq(true);
       });
 
       // ax arm
       const arm64x = hrefs.filter((x) => x.includes(FOLDER.arm));
       arm64x.forEach((x) => {
-        expect(x.endsWith('ax')).to.eq(true);
+        expect(x.endsWith(`${FOLDER.arm}ax`)).to.eq(true);
       });
     });
   });
