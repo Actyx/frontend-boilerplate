e252cb1a912aa96441ece88d785fb206d8ca53e0
GibboK <gibbok.coding@gmail.com>
2020-09-18 15:22:12 +0200

    Add test subscribe news letter)

--- cypress/integration/homepage.ts
+++ cypress/integration/homepage.ts
@@ -1,9 +1,13 @@
+import { mkRandomEmail } from '../utils';
+
 const URL_HOME = 'https://www.actyx.com/';
 
 describe('www.actyx.com/', () => {
-  it('should switch language after user clicks option', () => {
+  beforeEach(() => {
     cy.visit(URL_HOME);
+  });
 
+  it('should switch language after user clicks option', () => {
     // check if english appears as default
     cy.contains('English').should('exist');
     cy.contains('The Factory Digitization Platform').should('exist');
@@ -12,5 +16,22 @@ describe('www.actyx.com/', () => {
     cy.get('a[href="/de/*"]').first().click({ force: true });
     cy.contains('Deutsch').should('exist');
     cy.contains('Die Plattform zur Digitalisierung der Fabrik').should('exist');
+  });
+
+  it.only('should sign-up to our newsletter', () => {
+    // check if text is on page
+    const email = mkRandomEmail();
+    cy.contains('Sign-up to our newsletter').should('exist');
+
+    // get input and enter an email
+    const input = cy.get('input[type="email"]');
+    input.should('exist');
+    input.type(email);
+
+    // click submit button and assert subscription
+    const submit = cy.get('button[type="submit"]');
+    submit.should('exist');
+    submit.click();
+    cy.contains('Thank you! Your request was successfully submitted.');
   });
 });
