0c80cd46b12bb74967d30de43d6e161bb9bc48f1
GibboK <gibbok.coding@gmail.com>
2020-09-18 10:44:40 +0200

    check validate file extensions for android

--- cypress/integration/downloads.ts
+++ cypress/integration/downloads.ts
@@ -1,9 +1,22 @@
 const URL_START = 'https://downloads.actyx.com/';
 
 describe('downloads.actyx.com', () => {
-  it('should return 200 status', () => {
-    cy.request(URL_START).then((resp) => {
-      expect(resp.status).to.eq(200);
+  it('should check if artifact files name are valid', () => {
+    cy.visit(URL_START);
+    cy.get('a').then((x) => {
+      const hrefs = x
+        .toArray()
+        .map((x) => x.href)
+        .filter((x) => x !== '')
+        .filter((x) => x.startsWith('https://axartifacts.blob.core.windows.net/artifacts/'));
+
+      const android = hrefs.filter((x) => x.includes('android'));
+      android.forEach((x) => {
+        expect(x).to.includes('actyxos.apk');
+      });
+      console.log(android);
+
+      console.log(hrefs);
     });
   });
 });
--- cypress/integration/get-started.ts
+++ cypress/integration/get-started.ts
@@ -21,12 +21,6 @@ const submitForm = () => {
 };
 
 describe('actyx.com/get-started/', () => {
-  it('should return 200 status', () => {
-    cy.request(URL_START).then((resp) => {
-      expect(resp.status).to.eq(200);
-    });
-  });
-
   it('should schedule a call for a factory manager', () => {
     cy.visit(URL_START);
 
