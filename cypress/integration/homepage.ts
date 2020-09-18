a33ea280ca2910c10f7a259fce677534176e07ae
GibboK <gibbok.coding@gmail.com>
2020-09-18 15:54:39 +0200

    Add test close cookies warning

--- cypress/integration/homepage.ts
+++ cypress/integration/homepage.ts
@@ -1,6 +1,10 @@
 import { mkRandomEmail } from '../utils';
 
 const URL_HOME = 'https://www.actyx.com/';
+const MSG_PRIVACY_EN =
+  'This website stores cookies on your computer to provide more personalized services to you.';
+
+const MSG_PRIVACY_DE = 'Diese Website speichert Cookies auf Ihrem Computer.';
 
 describe('www.actyx.com/', () => {
   beforeEach(() => {
@@ -8,11 +12,11 @@ describe('www.actyx.com/', () => {
   });
 
   it('should switch language after user clicks option', () => {
-    // check if english appears as default
+    // check if en appears as default
     cy.contains('English').should('exist');
     cy.contains('The Factory Digitization Platform').should('exist');
 
-    // change to german
+    // change to de
     cy.get('a[href="/de/*"]').first().click({ force: true });
     cy.contains('Deutsch').should('exist');
     cy.contains('Die Plattform zur Digitalisierung der Fabrik').should('exist');
@@ -38,14 +42,19 @@ describe('www.actyx.com/', () => {
   describe.only('cookies warning', () => {
     it('should show cookies warning in en', () => {
       cy.visit(`${URL_HOME}`);
-      cy.contains(
-        'This website stores cookies on your computer to provide more personalized services to you.'
-      ).should('exist');
+      cy.contains(MSG_PRIVACY_EN).should('exist');
     });
 
     it('should show cookies warning in de', () => {
       cy.visit(`${URL_HOME}/de/`);
-      cy.contains('Diese Website speichert Cookies auf Ihrem Computer.').should('exist');
+      cy.contains(MSG_PRIVACY_DE).should('exist');
+    });
+
+    it('should close warning when user accepts cookies', () => {
+      cy.visit(`${URL_HOME}`);
+      const buttonAccept = cy.get('#hs-eu-confirmation-button').should('exist');
+      buttonAccept.click();
+      cy.contains(MSG_PRIVACY_EN).should('be.hidden');
     });
   });
 });
