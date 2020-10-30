1063586302f719aa03291e7cc7b3ee4633e04ee9
GibboK <gibbok.coding@gmail.com>
2020-10-30 15:14:32 +0100

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:react/recommended', // Enables eslint-plugin-react for React specific linting rules for ESLint.
   ],
   plugins: ['simple-import-sort'],
   parserOptions: {
@@ -24,10 +23,5 @@ module.exports = {
       },
     ],
     'simple-import-sort/sort': 'error',
-  },
-  settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
   },
 };
